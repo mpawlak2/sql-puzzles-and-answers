bdcd8af70f0bba82312b883d3b50538fbfdf77c0
Mateusz Pawlak <mpawlak62@gmail.com>
2016-08-03 20:35:42 +0200

    solved

--- 03_anesthesia.sql
+++ 03_anesthesia.sql
@@ -12,17 +12,34 @@ create table surgeries(proc_id int not null references procedures(id), anest_nam
 
 
 -- create view with every event (1 = start, -1 = end)
-create view events
+-- drop view events
+create view events(proc_id, comparison_proc, anest_name, event_time, state)
 as
-select proc_id, anest_name, 1 as event_type, start_time as event_time from surgeries
+select s.proc_id, s2.proc_id, s.anest_name, s2.start_time, +1 
+from surgeries s
+join surgeries s2 on s.anest_name = s2.anest_name and not (s2.end_time <= s.start_time or s2.start_time >= s.end_time)
 union
-select proc_id, anest_name, -1, end_time from surgeries
+select s.proc_id, s2.proc_id, s.anest_name, s2.end_time, -1 as state
+from surgeries s
+join surgeries s2 on s.anest_name = s2.anest_name and not (s2.end_time <= s.start_time or s2.start_time >= s.end_time)
 
 
--- assuming proc_id is unique
-select 
-	ee.proc_id, 
-	(select count(proc_id) from events ev where ev.event_time >= min(ee.event_time) and ev.event_time <= max(ee.event_time) and ev.proc_id <> ee.proc_id) 
-from events ee
-group by proc_id
+select e1.proc_id, (select sum(e2.state) from events e2 where e2.proc_id = e1.proc_id and e2.event_time < e1.event_time)
+from events e1
+order by e1.proc_id, e1.event_time
+-------------------^ book solution
 
+-- 1. sum event states (+1/-1) when event time < end_time
+create view working_periods
+as
+select s1.proc_id, s2.proc_id compare_to, s1.start_time, s2.end_time
+from surgeries s1, surgeries s2
+where not (s2.start_time >= s1.end_time or s2.end_time <= s1.start_time) and s1.anest_name = s2.anest_name
+
+select * from events
+
+select wp.proc_id, wp.compare_to, (select sum(e.state) from events e where e.proc_id = wp.proc_id and e.event_time < wp.end_time)
+from working_periods wp
+order by wp.proc_id
+
+select * from surgeries
