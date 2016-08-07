7c4295634abd114d44d678da4bbbf06d9d9f7669
Mateusz Pawlak <mpawlak62@gmail.com>
2016-08-07 20:24:59 +0200

    10 puzzle - done

--- 10_wage_of_sin.sql
+++ 10_wage_of_sin.sql
@@ -14,14 +14,26 @@ from pensions p
 left join pensions p2 on p.sin = p2.sin and p.pen_year > p2.pen_year
 order by p.sin, p.pen_year desc
 
-select p.sin, p.pen_year, max(p.month_cnt) month_cnt, max(p.month_cnt) + (select isnull(sum(month_cnt), 0) from pensions pp where pp.sin = p.sin and pp.pen_year > p.pen_year) as monts_so_far
+create view pension_periods(sin, from_year, month_cnt, sixty_earnings)
+as
+select pensions_m.sin, pensions_m.pen_year, pensions_m.month_cnt, pensions_m.earningsT + (pensions_m.earnings / pensions_m.month_cnt) * (month_cnt - (monts_so_far - 60))
+from(
+select 
+	p.sin, 
+	p.pen_year, 
+	max(p.month_cnt) month_cnt,
+	max(p.month_cnt) + (select isnull(sum(pp.month_cnt), 0) from pensions pp where pp.sin = p.sin and pp.pen_year > p.pen_year) as monts_so_far, 
+	(select isnull(sum(pp.earnings), 0) from pensions pp where pp.sin = p.sin and pp.pen_year > p.pen_year) earningsT,
+	(select pp.earnings from pensions pp where pp.sin = p.sin and pp.pen_year = p.pen_year) earnings
 from pensions p
 left join pensions p2 on p.sin = p2.sin and p.pen_year > p2.pen_year
 group by p.sin, p.pen_year
-order by p.sin, p.pen_year desc
+--order by p.sin, p.pen_year desc
+) pensions_m
+where (pensions_m.monts_so_far > 60) and (pensions_m.monts_so_far - pensions_m.month_cnt <= 60)
 
 
-
+select * from pension_periods
 
 
 ------------------------
